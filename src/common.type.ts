3f39d6c3708c1911dd27df00892fe67f2282009e
kiwi <kiwi71728@icloud.com>
2022-01-09 07:34:50 +0800

    chore: add rtk-query service

---
+++ src/common.type.ts
@@ -0,0 +1,35 @@
+interface AssetItem {
+  id: number;
+  tokenId: string;
+  imageUrl: string;
+  name: string;
+}
+
+export interface ApiOpenSeaAssetsResponse {
+  assets: AssetItem[];
+}
+
+export interface ApiOpenSeaAssetsRequest {
+  owner: string;
+  format: "json";
+  offset: number;
+  limit: number;
+}
+
+interface AssetCollection {
+  name: string;
+}
+
+export interface ApiOpenSeaAssetDetailResponse {
+  id: number;
+  tokenId: string;
+  imageUrl: string;
+  description: string;
+  collection: AssetCollection;
+  permalink: string;
+}
+
+export interface ApiOpenSeaAssetDetailRequest {
+  address: string;
+  tokenId: string;
+}
